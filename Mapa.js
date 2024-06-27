9fe02c59bfee3eed192180753d265658ca522b81
William <121244216+WilliamRC-x@users.noreply.github.com>
2024-06-27 17:15:07 -0300

    Add files via upload

---
+++ Mapa.js
@@ -0,0 +1,6 @@
+function soma(){
+    var caixa1 = document.getElementById("caixa1").value;
+    var caixa2 = document.getElementById("caixa2").value;
+    var caixa3 = document.getElementById("caixa3").value;
+    
+}
